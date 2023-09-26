5d792fb9ea5d76e5c17acf69e026f85648e9e095
iramoz95 <ian.ramoz95@gmail.com>
2023-09-26 08:19:59 -0600

    Remote state storage example

---
+++ chapter-3/remote_state_storage/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-state-irz"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-locks-irz"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ chapter-3/remote_state_storage/variables.tf
@@ -0,0 +1,17 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "ami_id" {
+  description = "The AMI to run in the cluster"
+  type        = string
+  default     = "ami-04cb4ca688797756f"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+  type        = string
+  default     = "t2.micro"
+}
